d4c0bafb0a702f23810b11cb13811500c275ff1e
SiLar92 <sindrla92@gmail.com>
2022-07-11 13:36:14 +0200

    sorter på tid og override hashmap og equals
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>
    Co-authored-by: Nora Evensen Jansrud <nora.evensen.jansrud@nav.no>
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>

--- spane-model/src/main/kotlin/no/nav/helse/Subsumsjon.kt
+++ spane-model/src/main/kotlin/no/nav/helse/Subsumsjon.kt
@@ -23,5 +23,56 @@ internal class Subsumsjon(
 ){
     companion object{
         fun List<Subsumsjon>.finnAlle(paragraf: String) = this.filter { it.paragraf == paragraf }
+
+        fun List<Subsumsjon>.sorterPåTid() = this.sortedBy { it.tidsstempel }
+
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Subsumsjon
+
+        if (id != other.id) return false
+        if (versjon != other.versjon) return false
+        if (eventName != other.eventName) return false
+        if (kilde != other.kilde) return false
+        if (versjonAvKode != other.versjonAvKode) return false
+        if (fødselsnummer != other.fødselsnummer) return false
+        if (sporing != other.sporing) return false
+        if (tidsstempel != other.tidsstempel) return false
+        if (lovverk != other.lovverk) return false
+        if (lovverksversjon != other.lovverksversjon) return false
+        if (paragraf != other.paragraf) return false
+        if (ledd != other.ledd) return false
+        if (punktum != other.punktum) return false
+        if (bokstav != other.bokstav) return false
+        if (input != other.input) return false
+        if (output != other.output) return false
+        if (utfall != other.utfall) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + versjon.hashCode()
+        result = 31 * result + eventName.hashCode()
+        result = 31 * result + kilde.hashCode()
+        result = 31 * result + versjonAvKode.hashCode()
+        result = 31 * result + fødselsnummer.hashCode()
+        result = 31 * result + sporing.hashCode()
+        result = 31 * result + tidsstempel.hashCode()
+        result = 31 * result + lovverk.hashCode()
+        result = 31 * result + lovverksversjon.hashCode()
+        result = 31 * result + paragraf.hashCode()
+        result = 31 * result + (ledd ?: 0)
+        result = 31 * result + (punktum ?: 0)
+        result = 31 * result + (bokstav?.hashCode() ?: 0)
+        result = 31 * result + input.hashCode()
+        result = 31 * result + output.hashCode()
+        result = 31 * result + utfall.hashCode()
+        return result
     }
 }
--- spane-model/src/test/kotlin/no/nav/helse/SubsumsjonTest.kt
+++ spane-model/src/test/kotlin/no/nav/helse/SubsumsjonTest.kt
@@ -1,6 +1,7 @@
 package no.nav.helse
 
 import no.nav.helse.Subsumsjon.Companion.finnAlle
+import no.nav.helse.Subsumsjon.Companion.sorterPåTid
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.time.*
@@ -16,37 +17,58 @@ internal class SubsumsjonTest {
     }
 
     @Test
-    fun `sorter på tid`(){
-        val subsumsjoner = listOf(lagSubsumsjon(), lagSubsumsjon("8-13"), lagSubsumsjon("8-13"))
-        assertEquals(0, 0)
+    fun `sorter på tid`() {
+        val subsumsjoner = listOf(
+            lagSubsumsjon(tidsstempel = 31.januar(2022)),
+            lagSubsumsjon(tidsstempel = 3.januar(2022)),
+            lagSubsumsjon(tidsstempel = 4.februar(2022))
+        )
+
+        val sortert = listOf(
+            lagSubsumsjon(tidsstempel = 3.januar(2022)),
+            lagSubsumsjon(tidsstempel = 31.januar(2022)),
+            lagSubsumsjon(tidsstempel = 4.februar(2022))
+        )
+        assertEquals(sortert, subsumsjoner.sorterPåTid())
 
     }
 
 
-
-
-
-
     fun lagSubsumsjon(paragraf: String = "8-11", tidsstempel: ZonedDateTime = 1.januar(2022)): Subsumsjon {
-        return Subsumsjon("id", "3", "sub", "kildee", "3",
+        return Subsumsjon(
+            "id", "3", "sub", "kildee", "3",
             "1234567890", emptyMap(), tidsstempel, "loven", "3",
             paragraf, null, null, null, emptyMap(), emptyMap(), "GODKJENT"
         )
     }
 
 
-
 }
 
-fun Int.januar(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 1, this, 0, 0), ZoneId.systemDefault())
-fun Int.februar(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 2, this, 0,0), ZoneId.systemDefault())
-fun Int.mars(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 3, this, 0,0), ZoneId.systemDefault())
-fun Int.april(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 4, this, 0,0), ZoneId.systemDefault())
-fun Int.mai(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 5, this, 0,0), ZoneId.systemDefault())
-fun Int.juni(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 6, this, 0,0), ZoneId.systemDefault())
-fun Int.juli(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 7, this, 0,0), ZoneId.systemDefault())
-fun Int.august(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 8, this, 0,0), ZoneId.systemDefault())
-fun Int.september(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 9, this, 0,0), ZoneId.systemDefault())
-fun Int.oktober(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 10, this, 0,0), ZoneId.systemDefault())
-fun Int.november(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 11, this, 0,0), ZoneId.systemDefault())
-fun Int.desember(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 12, this, 0,0), ZoneId.systemDefault())
+fun Int.januar(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 1, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.februar(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 2, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.mars(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 3, this, 0, 0), ZoneId.systemDefault())
+fun Int.april(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 4, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.mai(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 5, this, 0, 0), ZoneId.systemDefault())
+fun Int.juni(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 6, this, 0, 0), ZoneId.systemDefault())
+fun Int.juli(year: Int): ZonedDateTime = ZonedDateTime.of(LocalDateTime.of(year, 7, this, 0, 0), ZoneId.systemDefault())
+fun Int.august(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 8, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.september(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 9, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.oktober(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 10, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.november(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 11, this, 0, 0), ZoneId.systemDefault())
+
+fun Int.desember(year: Int): ZonedDateTime =
+    ZonedDateTime.of(LocalDateTime.of(year, 12, this, 0, 0), ZoneId.systemDefault())
