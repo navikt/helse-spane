83bc5f45b7196e32251cd9e013817279cd587327
Nora Evensen Jansrud <nora.evensen.jansrud@nav.no>
2022-07-15 14:32:14 +0200

    liksom- backend for lokal testing av frontend
    Co-authored-by: Sindre Larsen <sindre.larsen@nav.no>
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>

--- spane-visning/src/App.tsx
+++ spane-visning/src/App.tsx
@@ -1,36 +1,85 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
+import {Environment} from "./environment";
+import testPerson from "./testPerson.json"
+
+export const testBackend = (): Backend => {
+    return {
+        person(): Promise<PersonDto> {
+            return Promise.resolve(testPerson as unknown as PersonDto);
+        },
+    };
+};
+
+export type Backend = {
+    person: () => Promise<PersonDto>
+}
+
+export type PersonDto = {
+    vedtaksperioder: VedtaksperiodeDto[]
+    fnr: string
+}
+
+export type VedtaksperiodeDto = {
+    subsumsjoner: SubsumsjonDto[]
+}
+
+export type SubsumsjonDto = {
+    id: string
+    versjon: string
+    eventName: string
+    kilde: string
+    versjonAvKode: string
+    fødselsnummer: string
+    sporing: Map<string, string[]>
+    tidsstempel: string
+    lovverk: string
+    lovverksversjon: string
+    paragraf: string
+    ledd: number | null
+    punktum: number | null
+    bokstav: string | null
+    input: Map<string, any>
+    output: Map<string, any>
+    utfall: string
+}
 
 function App() {
-  const [person, setPerson] = useState<any>();
-  async function fetchAPI() {
-    await fetch("/fnr/10877799145")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setPerson(data);
-      });
-  }
+    const backend: Backend = Environment.isDevelopment
+        ? testBackend()
+        : testBackend();
 
-  useEffect(() => {
-    fetchAPI();
-  }, []);
 
-  return (
-    <>
-      <h1>no Spane no gain</h1>
-      <div>People call me the jarlinator, but you can call me tonight</div>
-      <div>
-        Liste over vedtaksperioder
-        {person
-          ? person.vedtaksperioder.map((info: any) => {
-              return <div style={{ display: "block" }}>{JSON.stringify(info)}</div>;
+    const [person, setPerson] = useState<PersonDto>();
+
+    async function fetchAPI() {
+        await fetch("/fnr/10877799145")
+            .then((res) => {
+                return res.json();
             })
-          : "fant ingen vedtaksperioder"}
-      </div>
-    </>
-  );
+            .then((data) => {
+                setPerson(data);
+            });
+    }
+
+    useEffect(() => {
+        backend.person().then(r => setPerson(r));
+    }, []);
+
+    return (
+        <>
+            <h1>no Spane no gain</h1>
+            <div>People call me the jarlinator, but you can call me tonight</div>
+            <div>
+                Liste over vedtaksperioder
+                {person
+                    ? person.vedtaksperioder.map((vedtaksperiode: VedtaksperiodeDto) => {
+                        return <div style={{display: "block"}}>{JSON.stringify(vedtaksperiode)}</div>;
+                    })
+                    : "fant ingen vedtaksperioder"}
+            </div>
+        </>
+    );
 }
 
 export default App;
