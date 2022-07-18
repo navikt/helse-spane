9aa05cc3e6ea37a165fa73b24d073433cdc88cd7
Sindre Larsen <sindre.larsen@nav.no>
2022-07-18 13:48:32 +0200

    Bytter useEffect med onSubmit, tidlig forsøk på å ta imot input
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>

--- spane-visning/src/App.tsx
+++ spane-visning/src/App.tsx
@@ -1,110 +1,118 @@
-import { useEffect, useState } from "react";
+import {FormEvent, useState} from "react";
 import "./App.css";
-import { Environment } from "./environment";
+import {Environment} from "./environment";
 import testPerson from "./resources/testPerson.json";
 import Vedtaksperiode from "./components/Vedtaksperiode";
 import "@navikt/ds-css";
 import "@navikt/ds-css-internal";
-import { Search } from "@navikt/ds-react";
+import {Search} from "@navikt/ds-react";
 
 export const restBackend = (): Backend => {
-  return {
-    person(): Promise<PersonDto> {
-      return fetch(`/fnr/10877799145`, {
-        method: "get",
-        headers: {
-          Accept: "application/json",
+    return {
+        person(fnr: string): Promise<PersonDto> {
+            return fetch(`/fnr/` + fnr, {
+                method: "get",
+                headers: {
+                    Accept: "application/json",
+                },
+            }).then((response) => response.json());
         },
-      }).then((response) => response.json());
-    },
-  };
+    };
 };
 
 export const testBackend = (): Backend => {
-  return {
-    person(): Promise<PersonDto> {
-      return Promise.resolve(testPerson as unknown as PersonDto);
-    },
-  };
+    return {
+        person(fnr: string): Promise<PersonDto> {
+            return Promise.resolve(testPerson as unknown as PersonDto);
+        },
+    };
 };
 
 export type Backend = {
-  person: () => Promise<PersonDto>;
+    person: (fnr: string) => Promise<PersonDto>;
 };
 
 export type PersonDto = {
-  vedtaksperioder: VedtaksperiodeDto[];
-  fnr: string;
+    vedtaksperioder: VedtaksperiodeDto[];
+    fnr: string;
 };
 
 export type VedtaksperiodeDto = {
-  subsumsjoner: SubsumsjonDto[];
+    subsumsjoner: SubsumsjonDto[];
 };
 
 export type SubsumsjonDto = {
-  id: string;
-  versjon: string;
-  eventName: string;
-  kilde: string;
-  versjonAvKode: string;
-  fødselsnummer: string;
-  sporing: Map<string, string[]>;
-  tidsstempel: string;
-  lovverk: string;
-  lovverksversjon: string;
-  paragraf: string;
-  ledd: number | null;
-  punktum: number | null;
-  bokstav: string | null;
-  input: Map<string, any>;
-  output: Map<string, any>;
-  utfall: string;
+    id: string;
+    versjon: string;
+    eventName: string;
+    kilde: string;
+    versjonAvKode: string;
+    fødselsnummer: string;
+    sporing: Map<string, string[]>;
+    tidsstempel: string;
+    lovverk: string;
+    lovverksversjon: string;
+    paragraf: string;
+    ledd: number | null;
+    punktum: number | null;
+    bokstav: string | null;
+    input: Map<string, any>;
+    output: Map<string, any>;
+    utfall: string;
 };
 
-function App() {
-  const backend: Backend = Environment.isDevelopment
-    ? testBackend()
-    : restBackend();
+function App(this: any) {
+    const backend: Backend = Environment.isDevelopment
+        ? testBackend()
+        : restBackend();
 
-  const [person, setPerson] = useState<PersonDto>();
-  const [fødselsnummer, setFødselsnummer] = useState<string>();
+    const [person, setPerson] = useState<PersonDto>();
+    const [fødselsnummer, setFødselsnummer] = useState<string>("");
 
-  useEffect(() => {
-    backend.person().then((r) => setPerson(r));
-  }, []);
+    // useEffect(() => {
+    //     backend.person().then((r) => setPerson(r));
+    // }, []);
 
-  return (
-    <>
-      <h1>no Spane no gain</h1>
-      <div>People call me the jarlinator, but you can call me tonight</div>
-      <div>
-        <Search
-          label="Søk alle NAV sine sider"
-          size="medium"
-          variant="secondary"
-          onChange={(e) => {
-            console.log(e);
-            setFødselsnummer(e);
-          }}
-        />
-      </div>
-      <div>
-        Liste over vedtaksperioder
-        {person
-          ? person.vedtaksperioder.map(
-              (vedtaksperiode: VedtaksperiodeDto, key) => {
-                return (
-                  <Vedtaksperiode
-                    key={key}
-                    subsumsjoner={vedtaksperiode.subsumsjoner}
-                  />
-                );
-              }
-            )
-          : "fant ingen vedtaksperioder"}
-      </div>
-    </>
-  );
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(fødselsnummer)
+        backend.person(fødselsnummer).then((r) => setPerson(r));
+    }
+
+    return (
+        <>
+            <h1>no Spane no gain</h1>
+            <div>People call me the jarlinator, but you can call me tonight</div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <Search
+                        label="Søk etter fødselsnummer"
+                        size="medium"
+                        variant="secondary"
+                        onChange={(e) =>
+                            setFødselsnummer(e)
+                        }
+                    />
+                </form>
+            </div>
+
+            <div>
+                Liste over vedtaksperioder
+                {person
+                    ? person.vedtaksperioder.map(
+                        (vedtaksperiode: VedtaksperiodeDto, key) => {
+                            return (
+                                <Vedtaksperiode
+                                    key={key}
+                                    subsumsjoner={vedtaksperiode.subsumsjoner}
+                                />
+                            );
+                        }
+                    )
+                    : "fant ingen vedtaksperioder"}
+            </div>
+        </>
+    );
 }
 
 export default App;
