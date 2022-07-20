cbcab880ad6db4bd391b736805435be26ddada4d
Nora Evensen Jansrud <nora.evensen.jansrud@nav.no>
2022-07-20 10:04:38 +0200

    klikkbar expandable row
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>

---
+++ spane-visning/src/components/SubsumsjonExpandableRow.tsx
@@ -0,0 +1,28 @@
+import { Table } from "@navikt/ds-react";
+import React, { useState } from "react";
+import { SubsumsjonDto } from "../types";
+import UtvidetTableInnhold from "./UtvidetTableInnhold";
+
+interface Props {
+  subsumsjon: SubsumsjonDto;
+  vedtaksperiodeIdx: number;
+}
+export default function SubsumsjonExpandableRow(props: Props) {
+  const { subsumsjon, vedtaksperiodeIdx } = props;
+  const [expand, setExpand] = useState<boolean>(false);
+  return (
+    <Table.ExpandableRow
+      content={<UtvidetTableInnhold subsumsjon={subsumsjon} />}
+      togglePlacement="right"
+      open={expand}
+      onClick={()=>setExpand(!expand)}
+      style = {{cursor:"pointer"}}
+    >
+      <Table.HeaderCell scope="row">{vedtaksperiodeIdx + 1}</Table.HeaderCell>
+      <Table.HeaderCell scope="row">{subsumsjon.paragraf}</Table.HeaderCell>
+      <Table.DataCell>{subsumsjon.fødselsnummer}</Table.DataCell>
+      <Table.DataCell>{subsumsjon.utfall}</Table.DataCell>
+      <Table.DataCell>{subsumsjon.tidsstempel}</Table.DataCell>
+    </Table.ExpandableRow>
+  );
+}
