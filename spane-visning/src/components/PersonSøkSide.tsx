a03c077b27d6fbea74e3baec55944d4132b1d6cf
Sindre Larsen <sindre.larsen@nav.no>
2022-08-01 13:16:29 +0200

    Hent ut orgnummer dynamisk til filterliste
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>

--- spane-visning/src/components/PersonSøkSide.tsx
+++ spane-visning/src/components/PersonSøkSide.tsx
@@ -1,45 +1,60 @@
-import { Search, Table } from "@navikt/ds-react";
-import React, { FormEvent, useState } from "react";
+import {Search, Table} from "@navikt/ds-react";
+import React, {FormEvent, useState} from "react";
 import VedtaksperiodeTableBody from "./VedtaksperiodeTableBody";
 import "@navikt/ds-css-internal";
-import { Backend, restBackend, testBackend } from "../service";
-import { Environment } from "../environment";
-import { PersonDto } from "../types";
+import {Backend, restBackend, testBackend} from "../service";
+import {Environment} from "../environment";
+import {PersonDto} from "../types";
 import "./personSøk.css";
 
-interface Props {}
+interface Props {
+    setOrgnumre: React.Dispatch<React.SetStateAction<string[]>>
+}
 
 function PersonSøkSide(props: Props) {
-  const {} = props;
-  const backend: Backend = Environment.isDevelopment
-    ? testBackend()
-    : restBackend();
 
-  const [person, setPerson] = useState<PersonDto>();
-  const [fødselsnummer, setFødselsnummer] = useState<string>("");
+    const {} = props;
+    const backend: Backend = Environment.isDevelopment
+        ? testBackend()
+        : restBackend();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    backend.person(fødselsnummer).then((r) => setPerson(r));
-  };
+    const [person, setPerson] = useState<PersonDto>();
+    const [fødselsnummer, setFødselsnummer] = useState<string>("");
 
-  return (
-    <main className="main-container">
-      <div className="search-container">
-        <form onSubmit={handleSubmit}>
-          <Search
-            label="Søk etter fødselsnummer"
-            size="medium"
-            variant="secondary"
-            onChange={(e) => setFødselsnummer(e)}
-          />
-        </form>
-      </div>
-      <Table size="medium">
-        {person && <VedtaksperiodeTableBody person={person} />}
-      </Table>
-    </main>
-  );
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        backend.person(fødselsnummer)
+            .then((r) => setPerson(r))
+            .then(()=>{
+                let orgnumre : string[] = []
+                person!.vedtaksperioder.forEach(
+                    (vedtaksperiode) => {
+                        if(!orgnumre.includes(vedtaksperiode.orgnummer)) {
+                            orgnumre.push(vedtaksperiode.orgnummer)
+                        }
+                    }
+                )
+                props.setOrgnumre(orgnumre)
+            });
+    };
+
+    return (
+        <main className="main-container">
+            <div className="search-container">
+                <form onSubmit={handleSubmit}>
+                    <Search
+                        label="Søk etter fødselsnummer"
+                        size="medium"
+                        variant="secondary"
+                        onChange={(e) => setFødselsnummer(e)}
+                    />
+                </form>
+            </div>
+            <Table size="medium">
+                {person && <VedtaksperiodeTableBody person={person}/>}
+            </Table>
+        </main>
+    );
 }
 
 export default PersonSøkSide;
--- spane-visning/src/components/Sidepanel.tsx
+++ spane-visning/src/components/Sidepanel.tsx
@@ -1,29 +1,32 @@
-import { Checkbox, CheckboxGroup } from "@navikt/ds-react";
+import {Checkbox, CheckboxGroup} from "@navikt/ds-react";
 import React from "react";
 import DatePicker from "./DatePicker";
 import "./sidepanel.css";
 
-interface Props {}
+interface Props {
+    orgnumre: string[]
+}
 
 function SidePanel(props: Props) {
-  const {} = props;
+    const {orgnumre} = props;
 
-  return (
-    <div className="sidepanel-container">
-      <h3>Personnummer: </h3>
-      12345678901
-      <div className="date-picker-container">
-        <DatePicker label="Fra" />
-        <DatePicker label="Til" />
-      </div>
-      <div className="checkbox-group-container">
-        <CheckboxGroup legend="Arbeidsgiver">
-          <Checkbox value={"123456"}>123456</Checkbox>
-          <Checkbox value={"98765"}> 98765</Checkbox>
-        </CheckboxGroup>
-      </div>
-    </div>
-  );
+    return (
+        <div className="sidepanel-container">
+            <h3>Personnummer: </h3>
+            12345678901
+            <div className="date-picker-container">
+                <DatePicker label="Fra"/>
+                <DatePicker label="Til"/>
+            </div>
+            <div className="checkbox-group-container">
+                <CheckboxGroup legend="Arbeidsgiver">
+                    {orgnumre.map(orgnummer => {
+                        return <Checkbox value={orgnummer}>{orgnummer}</Checkbox>
+                    })}
+                </CheckboxGroup>
+            </div>
+        </div>
+    );
 }
 
 export default SidePanel;
