c56efb57b0f60bd29f6eb9db10f73bc6706d2e8b
Sindre Larsen <sindre.larsen@nav.no>
2022-08-01 16:48:39 +0200

    Sorter liste av subsumsjoner i pvp på en veldig stygg måte
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>

--- spane-visning/src/components/UtvidetVedtaksperiodeTableInnhold.tsx
+++ spane-visning/src/components/UtvidetVedtaksperiodeTableInnhold.tsx
@@ -7,41 +7,62 @@ interface Props {
     subsumsjoner: SubsumsjonDto[];
 }
 
-var muligeUtfall = {
-    "VILKAR_OPPFYLT": 0,
-    "VILKAR_BEREGNET": 1,
-    "VILKAR_IKKE_OPPFYLT": 2,
-    "VILKAR_UAVKLART": 3,
-}
-
-
 function UtvidetVedtaksperiodeTableInnhold(props: Props) {
     const {subsumsjoner} = props
 
     const [sorterteSubsumsjoner, setSorterteSubsumsjoner] = useState(subsumsjoner)
 
+    function utfallStringVerdi(str: string) {
+        switch (str) {
+            case "VILKAR_OPPFYLT":
+                return 0
+            case "VILKAR_BEREGNET":
+                return 1
+            case "VILKAR_UAVKLART":
+                return 2
+            case "VILKAR_IKKE_OPPFYLT":
+                return 3
+            default:
+                return 4
+        }
+    }
+
     function handleSort(sortKey: string | undefined) {
         if (!sortKey) return;
         if (sortKey === "paragraf") {
-        setSorterteSubsumsjoner([...sorterteSubsumsjoner].sort((v1, v2) => {
-            let kapittel1 = parseInt(v1.paragraf.split("-")[0])
-            let kapittel2 = parseInt(v2.paragraf.split("-")[0])
-
-            if (kapittel1 == kapittel2) {
-                let paragraf1 = parseInt(v1.paragraf.split("-")[1])
-                let paragraf2 = parseInt(v2.paragraf.split("-")[1])
-
-                if (paragraf1 == paragraf2) {
-                    // TODO: Sorter videre på ledd og punktum
-                   return 0
-                } else return (paragraf1 > paragraf2) ? 1 : -1
-            } else return (kapittel1 > kapittel2) ? 1 : -1
-        }))
-        }
-        else if (sortKey === "utfall") {
             setSorterteSubsumsjoner([...sorterteSubsumsjoner].sort((v1, v2) => {
-                    // @ts-ignore
-                return ((muligeUtfall[v1.utfall]) > muligeUtfall(v2.utfall)) ? 1 : -1
+                let kapittel1 = parseInt(v1.paragraf.split("-")[0])
+                let kapittel2 = parseInt(v2.paragraf.split("-")[0])
+
+                if (kapittel1 == kapittel2) {
+                    let paragraf1 = parseInt(v1.paragraf.split("-")[1])
+                    let paragraf2 = parseInt(v2.paragraf.split("-")[1])
+
+                    if (paragraf1 == paragraf2) {
+                        let ledd1 = v1.ledd
+                        let ledd2 = v2.ledd
+
+                        if (ledd1 == ledd2) {
+
+                            let punktum1 = v1.punktum
+                            let punktum2 = v2.punktum
+
+                            if (punktum1 == punktum2) {
+
+                                let bokstav1 = v1.bokstav
+                                let bokstav2 = v2.bokstav
+
+                                if (bokstav1 != "null" && bokstav2 != "null") {
+                                    return (bokstav1 > bokstav2) ? 1 : -1
+                                } else return 0 // Hvis en av bokstavene er tilstede er den etter den uten
+                            } else return (punktum1 > punktum2) ? 1 : -1
+                        } else return (ledd1 > ledd2) ? 1 : -1
+                    } else return (paragraf1 > paragraf2) ? 1 : -1
+                } else return (kapittel1 > kapittel2) ? 1 : -1
+            }))
+        } else if (sortKey === "utfall") {
+            setSorterteSubsumsjoner([...sorterteSubsumsjoner].sort((v1, v2) => {
+                    return utfallStringVerdi(v1.utfall) > utfallStringVerdi(v2.utfall) ? 1 : -1
                 }
             ))
         }
