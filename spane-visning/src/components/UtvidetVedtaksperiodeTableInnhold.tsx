de4d0489b3e139fccd43603f217ea39d1ba8c79a
Sindre Larsen <sindre.larsen@nav.no>
2022-08-01 17:16:45 +0200

    Rett opp i null-verdier i testdata oppdater sorteringsfunksjon
    Co-authored-by: Jarl Sondre Sæther <jarl_sondre@hotmail.com>

--- spane-visning/src/components/UtvidetVedtaksperiodeTableInnhold.tsx
+++ spane-visning/src/components/UtvidetVedtaksperiodeTableInnhold.tsx
@@ -27,38 +27,44 @@ function UtvidetVedtaksperiodeTableInnhold(props: Props) {
         }
     }
 
+    function sorterPåParagraf(subsumsjonA: SubsumsjonDto, subsumsjonB: SubsumsjonDto): number {
+
+        let kapittel1 = parseInt(subsumsjonA.paragraf.split("-")[0])
+        let kapittel2 = parseInt(subsumsjonB.paragraf.split("-")[0])
+
+        if (kapittel1 == kapittel2) {
+            let paragraf1 = parseInt(subsumsjonA.paragraf.split("-")[1])
+            let paragraf2 = parseInt(subsumsjonB.paragraf.split("-")[1])
+
+            if (paragraf1 == paragraf2) {
+                let ledd1 = subsumsjonA.ledd ?? 0
+                let ledd2 = subsumsjonB.ledd ?? 0
+
+                if (ledd1 == ledd2) {
+
+                    let punktum1 = subsumsjonA.punktum ?? 0
+                    let punktum2 = subsumsjonB.punktum ?? 0
+
+                    if (punktum1 == punktum2) {
+
+                        let bokstav1 = subsumsjonA.bokstav ?? 'a'
+                        let bokstav2 = subsumsjonB.bokstav ?? 'a'
+
+                        if (bokstav1 != null && bokstav2 != null) {
+                            return (bokstav1 > bokstav2) ? 1 : -1
+                        } else return 0 // Hvis en av bokstavene er tilstede er den etter den uten
+                    } else return punktum1 - punktum2
+                } else return (ledd1 - ledd2)
+            } else return (paragraf1 - paragraf2)
+        } else return (kapittel1 - kapittel2)
+
+    }
+
     function handleSort(sortKey: string | undefined) {
         if (!sortKey) return;
         if (sortKey === "paragraf") {
             setSorterteSubsumsjoner([...sorterteSubsumsjoner].sort((v1, v2) => {
-                let kapittel1 = parseInt(v1.paragraf.split("-")[0])
-                let kapittel2 = parseInt(v2.paragraf.split("-")[0])
-
-                if (kapittel1 == kapittel2) {
-                    let paragraf1 = parseInt(v1.paragraf.split("-")[1])
-                    let paragraf2 = parseInt(v2.paragraf.split("-")[1])
-
-                    if (paragraf1 == paragraf2) {
-                        let ledd1 = v1.ledd
-                        let ledd2 = v2.ledd
-
-                        if (ledd1 == ledd2) {
-
-                            let punktum1 = v1.punktum
-                            let punktum2 = v2.punktum
-
-                            if (punktum1 == punktum2) {
-
-                                let bokstav1 = v1.bokstav
-                                let bokstav2 = v2.bokstav
-
-                                if (bokstav1 != "null" && bokstav2 != "null") {
-                                    return (bokstav1 > bokstav2) ? 1 : -1
-                                } else return 0 // Hvis en av bokstavene er tilstede er den etter den uten
-                            } else return (punktum1 > punktum2) ? 1 : -1
-                        } else return (ledd1 > ledd2) ? 1 : -1
-                    } else return (paragraf1 > paragraf2) ? 1 : -1
-                } else return (kapittel1 > kapittel2) ? 1 : -1
+                return sorterPåParagraf(v1, v2)
             }))
         } else if (sortKey === "utfall") {
             setSorterteSubsumsjoner([...sorterteSubsumsjoner].sort((v1, v2) => {
