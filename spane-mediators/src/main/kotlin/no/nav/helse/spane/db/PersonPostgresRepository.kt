b21871e16e4ef9fa42e339b223b784f0114a4461
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-08-03 15:21:40 +0200

    Oppdater person i databasen når den allerede eksisterer
    Co-authored-by: Sindre Larsen <sindre.larsen@nav.no>
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>

--- spane-mediators/src/main/kotlin/no/nav/helse/spane/db/PersonPostgresRepository.kt
+++ spane-mediators/src/main/kotlin/no/nav/helse/spane/db/PersonPostgresRepository.kt
@@ -13,7 +13,10 @@ internal class PersonPostgresRepository(private val dataSource: DataSource) : Pe
     }
 
     override fun lagre(json: String, fnr: String) {
-        val statement = "INSERT INTO person (fnr, data) VALUES (?, ?::JSON)"
+        val statement = "INSERT INTO person (fnr, data) VALUES (?, ?::JSON)" +
+                " ON CONFLICT (fnr)" +
+                " DO" +
+                " UPDATE SET data = EXCLUDED.data"
         sessionOf(dataSource).use { session ->
             session.run(queryOf(statement, fnr, json).asUpdate)
         }
