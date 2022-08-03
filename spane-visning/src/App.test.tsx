bf926d88f7f42cdadb66c27dbf92a55b86d3b678
Helene Arnesen <helene.arnesen@nav.no>
2022-08-03 12:16:06 +0200

    la tilbake test

---
+++ spane-visning/src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {byggStringRekursivt} from "./utlis";
+
+test('input blir parset rett av byggStringRekursivt', async () => {
+    const json = require('./resources/testInput.json');
+
+    console.log(byggStringRekursivt(json))
+
+
+})
