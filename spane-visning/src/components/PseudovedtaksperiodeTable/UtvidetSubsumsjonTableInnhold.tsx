f030fe98fbabda3eec707eeab993e608e223c0d7
Sindre Larsen <sindre.larsen@nav.no>
2022-08-04 16:49:25 +0200

    Vis teknisk info:
    - Subsumsjons ID
    - Versjon av kode
    - Versjon (av subsumsjon?)

--- spane-visning/src/components/PseudovedtaksperiodeTable/UtvidetSubsumsjonTableInnhold.tsx
+++ spane-visning/src/components/PseudovedtaksperiodeTable/UtvidetSubsumsjonTableInnhold.tsx
@@ -1,60 +1,79 @@
-import { SubsumsjonDto } from "../../types";
-import { FileContent } from "@navikt/ds-icons";
-import React, { useEffect, useState } from "react";
-import { byggStringRekursivt } from "../../utlis";
+import {SubsumsjonDto} from "../../types";
+import {FileContent} from "@navikt/ds-icons";
+import React, {useEffect, useState} from "react";
+import {byggStringRekursivt} from "../../utlis";
+import {ReadMore} from "@navikt/ds-react";
 
 interface Props {
-  subsumsjon: SubsumsjonDto;
-  anonymisert: Boolean
+    subsumsjon: SubsumsjonDto;
+    anonymisert: Boolean
 }
 
 export default function UtvidetSubsumsjonTableInnhold(props: Props) {
-  const { subsumsjon, anonymisert } = props;
-  const [outputString, setOutputString] = useState<string>("");
-  const [inputString, setInputString] = useState<string>("");
-  const [sporingString, setSporingString] = useState<string>("");
-  useEffect(() => {
-    setOutputString(byggStringRekursivt(subsumsjon.output));
-    setInputString(byggStringRekursivt(subsumsjon.input));
-    setSporingString(byggStringRekursivt(subsumsjon.sporing));
-  }, []);
+    const {subsumsjon, anonymisert} = props;
+    const [outputString, setOutputString] = useState<string>("");
+    const [inputString, setInputString] = useState<string>("");
+    const [sporingString, setSporingString] = useState<string>("");
+    useEffect(() => {
+        setOutputString(byggStringRekursivt(subsumsjon.output));
+        setInputString(byggStringRekursivt(subsumsjon.input));
+        setSporingString(byggStringRekursivt(subsumsjon.sporing));
+    }, []);
 
-  return (
-    <div className="table-row-expanded-content-container">
-      <div className="table-row-expanded-content-column">
-      <div>
-          <b>Faktum: </b>
-          {!anonymisert ? inputString.split("\n").map((s, key) => (
-            <div style={{ overflowWrap: "break-word" }} key={key}>
-              {s}
+    function tekniskInfo() {
+        let resultatString = ""
+        resultatString += "Subsumsjons ID:\n " + subsumsjon.id + "\n"
+        resultatString += "Versjon av kode:\n " + subsumsjon.versjonAvKode + "\n"
+        resultatString += "Versjon:\n " + subsumsjon.versjon + "\n"
+        return resultatString.split("\n").map((s, key) => (
+            <div style={{overflowWrap: "break-word"}} key={key}>
+                &nbsp;{s}
             </div>
-          )) : '********'}
+        ))
+    }
+
+    return (
+        <div className="table-row-expanded-content-container">
+            <div className="table-row-expanded-content-column">
+                <div>
+                    <b>Faktum: </b>
+                    {!anonymisert ? inputString.split("\n").map((s, key) => (
+                        <div style={{overflowWrap: "break-word"}} key={key}>
+                            &nbsp;{s}
+                        </div>
+                    )) : '********'}
+                </div>
+                <div>
+                    <b>Resultat: </b>
+                    {!anonymisert ? outputString.split("\n").map((s, key) => (
+                        <div key={key}>&nbsp;{s}</div>
+                    )) : '********'}
+                </div>
+            </div>
+            <div className="table-row-expanded-content-column">
+                <div>
+                    <b>Kilde: </b>
+                    {subsumsjon.kilde}
+                </div>
+                <div>
+                    <b>Lovverk: </b> {subsumsjon.lovverk}
+                </div>
+                <div>
+                    <b>Versjon av lovverk: </b> {subsumsjon.lovverksversjon}
+                </div>
+                <div>
+                    <p></p>
+                    {sporingString.split("\n").filter((s) => (s === "sykmelding:" || s === "soknad:")).map((s, key) => (
+                        <div key={key}> &nbsp;{s === "soknad:" ? "søknad:" : s}<FileContent/></div>
+                    ))}
+                </div>
+                <ReadMore
+                    size="small"
+                    header="Teknisk info"
+                >
+                    {tekniskInfo()}
+                </ReadMore>
+            </div>
         </div>
-        <div>
-          <b>Resultat: </b>
-          {!anonymisert ? outputString.split("\n").map((s, key) => (
-            <div key={key}>{s}</div>
-          )) : '********'}
-        </div>
-      </div>
-      <div className="table-row-expanded-content-column">
-        <div>
-          <b>Kilde: </b>
-          {subsumsjon.kilde}
-        </div>
-        <div>
-          <b>Lovverk: </b> {subsumsjon.lovverk}
-        </div>
-        <div>
-          <b>Versjon av lovverk: </b> {subsumsjon.lovverksversjon}
-        </div>
-        <div>
-          <p></p>
-          {sporingString.split("\n").filter((s) => (s === "sykmelding:" || s === "soknad:")).map((s, key) => (
-            <div key={key} > {s === "soknad:" ? "søknad:": s}<FileContent/></div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
