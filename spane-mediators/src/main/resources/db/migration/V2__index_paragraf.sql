4bf7edd30841c9ba9499f91df1273a1b2a528d5a
Sindre Larsen <sindre.larsen@nav.no>
2022-08-08 15:33:47 +0200

    Dropper flyway

--- spane-mediators/src/main/resources/db/migration/V2__index_paragraf.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE person
-(
-    fnr  VARCHAR(32) NOT NULL,
-    data JSON        NOT NULL,
-    PRIMARY KEY (fnr)
-);
-
-CREATE INDEX idx_person_paragraf
-    ON person USING BTREE ((data ->'vedtaksperioder'->'subsumsjoner' ->> 'paragraf'));
