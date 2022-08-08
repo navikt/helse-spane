1c7087942dda3e73a7826e202335275304a5c8ac
Sindre Larsen <sindre.larsen@nav.no>
2022-08-08 09:08:15 +0200

    Bytt til Tab i stedet for ToggleGroup for å matche speil

--- spane-visning/src/components/MainContainer.tsx
+++ spane-visning/src/components/MainContainer.tsx
@@ -9,7 +9,7 @@ import { Header } from "@navikt/ds-react-internal";
 import { ParagrafsøkDto, PersonDto } from "../types";
 import { Environment } from "../environment";
 import PseudovedtaksperiodeTabell from "./Tabell/PseudovedtaksperiodeTabell";
-import { ToggleGroup } from "@navikt/ds-react";
+import { Tabs } from "@navikt/ds-react";
 import { Law, People } from "@navikt/ds-icons";
 import PersonTabell from "./Tabell/PersonTabell";
 
@@ -31,7 +31,6 @@ export default function MainContainer() {
   const [paragrafSøk, setParagrafSøk] = useState<ParagrafsøkDto>();
   const [anonymisert, setAnonymisert] = useState<Boolean>(false);
   const [søk, setSøk] = useState<string>("");
-  const [fane, setFane] = useState<string>("Person");
 
   const backendPerson: BackendPerson = Environment.isDevelopment
     ? testBackendPerson()
@@ -45,63 +44,94 @@ export default function MainContainer() {
       <Header>
         <Header.Title as="h1">Paragraf i kode</Header.Title>
       </Header>
-
       <div>
-        <ToggleGroup onChange={(x) => setFane(x)} value={fane} size="medium">
-          <ToggleGroup.Item value="Person">
-            <People aria-hidden />
-            Søk på person
-          </ToggleGroup.Item>
-          <ToggleGroup.Item value="Paragraf">
-            <Law aria-hidden />
-            Søk på paragraf
-          </ToggleGroup.Item>
-        </ToggleGroup>
-      </div>
-
-      <div>
-        <div className="ytre-main-container">
-          <Søkefelt
-            fødselsnummer={søk}
-            backendPerson={backendPerson}
-            backendParagraf={backendParagraf}
-            setOrgnumre={setOrgnumre}
-            setSøk={setSøk}
-            setPerson={setPerson}
-            setPersoner={setParagrafSøk}
-            setAnonymisert={setAnonymisert}
-            anonymisert={anonymisert}
-            fane={fane}
-          />
-          <div className="indre-main-container">
-            <Sidepanel
-              orgnumre={orgnumre}
-              setValgte={setValgte}
-              setFraDato={setFraDato}
-              setTilDato={setTilDato}
-              søk={søk}
-              person={person}
-              anonymisert={anonymisert}
-              fane={fane}
+        <Tabs defaultValue="Person" size="medium">
+          <Tabs.List>
+            <Tabs.Tab
+              value="Person"
+              iconPosition="left"
+              label="Person"
+              icon={<People title="Søk på person" />}
             />
-            <div className={"tabell-container"}>
-              {fane === "Person" ? (
-                <PseudovedtaksperiodeTabell
-                  valgte={valgte}
-                  fraDato={fraDato}
-                  tilDato={tilDato}
+            <Tabs.Tab
+              value="Paragraf"
+              iconPosition="left"
+              label="Paragraf"
+              icon={<Law title="Søk på paragraf" />}
+            />
+          </Tabs.List>
+          <Tabs.Panel value="Person" className="h-24 w-full bg-gray-50 p-8">
+            <div className="ytre-main-container">
+              <Søkefelt
+                fødselsnummer={søk}
+                backendPerson={backendPerson}
+                backendParagraf={backendParagraf}
+                setOrgnumre={setOrgnumre}
+                setSøk={setSøk}
+                setPerson={setPerson}
+                setPersoner={setParagrafSøk}
+                setAnonymisert={setAnonymisert}
+                anonymisert={anonymisert}
+                fane={"Person"}
+              />
+              <div className="indre-main-container">
+                <Sidepanel
+                  orgnumre={orgnumre}
+                  setValgte={setValgte}
+                  setFraDato={setFraDato}
+                  setTilDato={setTilDato}
+                  søk={søk}
                   person={person}
                   anonymisert={anonymisert}
+                  fane={"Person"}
                 />
-              ) : (
-                <PersonTabell
-                  paragrafSøk={paragrafSøk}
-                  anonymisert={anonymisert}
-                />
-              )}
+                <div className={"tabell-container"}>
+                  <PseudovedtaksperiodeTabell
+                    valgte={valgte}
+                    fraDato={fraDato}
+                    tilDato={tilDato}
+                    person={person}
+                    anonymisert={anonymisert}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </Tabs.Panel>
+          <Tabs.Panel value="Paragraf" className="h-24 w-full bg-gray-50 p-8">
+            <div className="ytre-main-container">
+              <Søkefelt
+                fødselsnummer={søk}
+                backendPerson={backendPerson}
+                backendParagraf={backendParagraf}
+                setOrgnumre={setOrgnumre}
+                setSøk={setSøk}
+                setPerson={setPerson}
+                setPersoner={setParagrafSøk}
+                setAnonymisert={setAnonymisert}
+                anonymisert={anonymisert}
+                fane={"Paragraf"}
+              />
+              <div className="indre-main-container">
+                <Sidepanel
+                  orgnumre={orgnumre}
+                  setValgte={setValgte}
+                  setFraDato={setFraDato}
+                  setTilDato={setTilDato}
+                  søk={søk}
+                  person={person}
+                  anonymisert={anonymisert}
+                  fane={"Paragraf"}
+                />
+                <div className={"tabell-container"}>
+                  <PersonTabell
+                    paragrafSøk={paragrafSøk}
+                    anonymisert={anonymisert}
+                  />
+                </div>
+              </div>
+            </div>
+          </Tabs.Panel>
+        </Tabs>
       </div>
     </div>
   );
