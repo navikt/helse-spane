e163f8266b553df9f4b3a0e0f4c9231a47c53052
david steinsland <david@davidsteinsland.net>
2023-11-14 11:59:22 +0100

    prøver å fikse avhengighet mellom tasks

--- spane-mediators/build.gradle.kts
+++ spane-mediators/build.gradle.kts
@@ -15,7 +15,7 @@ tasks {
 
     jar {
         archiveFileName.set("app.jar")
-        mustRunAfter(":spane-visning:compileJava",":spane-visning:npm_run_build")
+        mustRunAfter(":spane-visning:compileTestJava",":spane-visning:compileJava",":spane-visning:jar",":spane-visning:npm_run_build")
 
 
         manifest {
