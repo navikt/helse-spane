d9ef425e2a15d43aabe69217dce334e0e66783b7
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-11-29 21:22:37 +0100

    ✏️ Feil databasebrukernavn

--- spane-mediators/src/main/resources/db/migration/V5__tilganger_for_spane-opprydding.sql
+++ spane-mediators/src/main/resources/db/migration/V5__tilganger_for_spane-opprydding.sql
@@ -2,8 +2,8 @@ DO
 $$
     BEGIN
         IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'spane-opprydding') THEN
-            GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "spedisjon-opprydding";
-            GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "spedisjon-opprydding";
+            GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "spane-opprydding";
+            GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "spane-opprydding";
         END IF;
     END
 $$
