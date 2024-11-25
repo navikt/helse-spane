db86134443f30f0ce693d90a2a524d74fa077dc6
david steinsland <david@davidsteinsland.net>
2024-11-25 10:29:16 +0100

    bumper tbd-libs

--- spane-opprydding/build.gradle.kts
+++ spane-opprydding/build.gradle.kts
@@ -4,7 +4,7 @@ val hikariCPVersion = "6.1.0"
 private val kotliqueryVersion = "1.9.0"
 private val flywayVersion = "10.21.0"
 private val rapidsAndRiversVersion = "2024112510241732526640.8542991368ca"
-val tbdLibsVersion = "2024.11.24-12.01-42fdc22d"
+val tbdLibsVersion = "2024.11.25-10.23-348b187b"
 val junitJupiterVersion = "5.11.3"
 
 dependencies {
