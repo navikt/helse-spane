893d4bc3242123cb9da553aa626a44b8221e02ed
david steinsland <david@davidsteinsland.net>
2025-01-27 13:08:44 +0100

    bumper r&r
    se commit for viktig endring:
    https://github.com/navikt/tbd-libs/commit/17a27722b0843e4c1b3cd16aeada837126b6124b

--- spane-opprydding/build.gradle.kts
+++ spane-opprydding/build.gradle.kts
@@ -3,7 +3,7 @@ private val postgresqlVersion = "42.7.4"
 val hikariCPVersion = "6.1.0"
 private val kotliqueryVersion = "1.9.0"
 private val flywayVersion = "10.21.0"
-private val rapidsAndRiversVersion = "2024120608421733470968.fc0a3d5e3685"
+private val rapidsAndRiversVersion = "2025012712551737978926.de930d8e0feb"
 val tbdLibsVersion = "2024.11.25-10.59-6f263a10"
 val junitJupiterVersion = "5.11.3"
 
